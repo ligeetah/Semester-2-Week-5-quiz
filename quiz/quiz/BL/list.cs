d9411093d6e07bde75bb1cc857f9ce7b0619e40c
Abdul Mateen <98536971+I-am-Mateen@users.noreply.github.com>
2022-04-20 21:12:58 +0500

    Add files via upload

---
+++ quiz/quiz/BL/list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using quiz.BL;
+namespace quiz.BL
+{
+    internal class list
+    {
+        public test marble;
+        public test granite;
+        public list() { }
+        public list(int t1, int t2, int t3, int t4, int t5, int t6)
+        {
+            marble=new test(t1,t2,t3);
+            granite=new test(t4,t5,t6);
+        }
+        public void total()
+        {
+            marble.cal_total();
+            granite.cal_total();
+        }
+    }
+}
---
+++ quiz/quiz/BL/test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz.BL
+{
+    internal class test
+    {
+        public int tile_cost;
+        public int installation_cost;
+        public int polishing_cost;
+        public double total;
+        public test() { }
+        public test(int t1, int t2, int t3)
+        {
+            tile_cost = t1;
+            installation_cost = t2;
+            polishing_cost = t3;
+        }
+        public void cal_total()
+        {
+            total = tile_cost + installation_cost + polishing_cost;
+        }
+    }
+}
